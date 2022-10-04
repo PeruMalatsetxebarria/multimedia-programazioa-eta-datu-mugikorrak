1de55afcc9caa5afdb7d5f8a8e974ffcdf3a9fad
Peru Malatsetxebarria <malatse.peru@uni.eus>
2022-10-04 11:41:46 +0200

    aaaaaaaag

--- app/src/main/java/com/example/helloworld/MainActivity2.java
+++ app/src/main/java/com/example/helloworld/MainActivity2.java
@@ -2,7 +2,9 @@ package com.example.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void junbestera2(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
